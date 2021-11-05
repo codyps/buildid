af3d1d37092a796d31193200be4cde8792661e6e
Cody P Schafer <dev@codyps.com>
2021-11-05 02:55:03 -0400

    windows works

--- src/windows.rs
+++ src/windows.rs
@@ -1,63 +1,48 @@
 use lazy_static::lazy_static;
-use object::Object;
 use tracing::{event, Level};
+use winapi::um::dbghelp::ImageNtHeader;
 use winapi::um::libloaderapi::GetModuleHandleA;
-use winapi::um::psapi::GetModuleInformation;
+use winapi::um::winnt::IMAGE_DEBUG_DIRECTORY;
+use winapi::um::winnt::IMAGE_DEBUG_TYPE_CODEVIEW;
+use winapi::um::winnt::IMAGE_DIRECTORY_ENTRY_DEBUG;
+
+#[allow(bad_style)]
+struct CV_INFO_PDB70 {
+    cv_signature: u32,
+    signature: [u8; 16],
+    _age: u32,
+    // followed by pdb name
+}
 
 lazy_static! {
     // This primarily exists as a hack to allow us to return a `&'static [u8]`
     static ref BUILD_ID_CACHE: Option<&'static [u8]> = {
         let module = unsafe { GetModuleHandleA(core::ptr::null_mut()) };
-        let mut module_info = core::mem::MaybeUninit::uninit();
+        event!(Level::TRACE, "module {:#x}", module as usize);
 
-        // NOTE: GetModuleInformation is just using the PE header to get it's info. We can probably
-        // skip using it entirely if we grab the size ourselves or `object` can handle it for us.
-        let ok = unsafe {
-            GetModuleInformation(
-                core::ptr::null_mut(),
-                module,
-                module_info.as_mut_ptr(),
-                core::mem::size_of_val(&module_info) as u32,
-            )
-        };
-        if ok != 0 {
+        let image_nt_header = unsafe { &*ImageNtHeader(module as _) };
+
+        let opt_header = &image_nt_header.OptionalHeader;
+        let dir = &opt_header.DataDirectory[IMAGE_DIRECTORY_ENTRY_DEBUG as usize];
+        if dir.Size == 0 {
+            event!(Level::ERROR, "IMAGE_DIRECTORY_ENTRY_DEBUG is empty");
             return None;
         }
 
-        let module_info = unsafe { module_info.assume_init() };
+        let dbg_dir = unsafe { &*((module as usize + dir.VirtualAddress as usize) as *const IMAGE_DEBUG_DIRECTORY) };
 
-        let module_slice: &'static [u8] = unsafe {
-            core::slice::from_raw_parts(
-                module_info.lpBaseOfDll as *const u8,
-                module_info.SizeOfImage as usize,
-            )
-        };
-
-        // parse the PE file that starts at handle to locate a gnu build-id section or some windows
-        // thingy.
-
-        // let v = match object::read::File::parse(module_slice) {
-        let v: object::read::pe::PeFile<'_, object::pe::ImageNtHeaders64> = match object::read::pe::PeFile::parse(module_slice) {
-            Err(e) => {
-                event!(Level::ERROR, "module parse failed: {}", e);
-                return None;
+        if dbg_dir.Type == IMAGE_DEBUG_TYPE_CODEVIEW {
+            let pdb_info = unsafe { &*((module as usize + dbg_dir.AddressOfRawData as usize) as *const CV_INFO_PDB70) };
+            if pdb_info.cv_signature != 0x53445352 {
+                event!(Level::ERROR, "mismatch sig: {:#x}", pdb_info.cv_signature);
+                None
+            } else {
+                Some(&pdb_info.signature[..])
             }
-            Ok(v) => v,
-        };
-
-        let cv = match v.pdb_info() {
-            Err(e) => {
-                event!(Level::ERROR, "error obtaining CodeView: {}", e);
-                return None;
-            }
-            Ok(None) => {
-                event!(Level::INFO, "no pdb_info present in executable");
-                return None;
-            }
-            Ok(Some(v)) => v,
-        };
-
-        Some(&Box::leak(Box::new(cv.guid()))[..])
+        } else {
+            event!(Level::ERROR, "wrong image type {:#x}", dbg_dir.Type);
+            None
+        }
     };
 }
 
