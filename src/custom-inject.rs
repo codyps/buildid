456782a8654bd93c3ba3121577d0b415a42f8edb
Cody P Schafer <dev@codyps.com>
2021-11-08 21:35:39 -0500

    clippy

--- src/custom-inject.rs
+++ src/custom-inject.rs
@@ -9,15 +9,17 @@ pub fn build_id() -> Option<&'static [u8]> {
     let mut l = MaybeUninit::<usize>::uninit();
     let r = unsafe { build_id__get(b.as_mut_ptr(), l.as_mut_ptr()) };
 
-    if r < 0 {
-        log::error!("build_id__get returned error: {}", r);
-        None
-    } else if r == 0 {
-        None
-    } else {
-        let b = unsafe { b.assume_init() };
-        let l = unsafe { l.assume_init() };
+    match r {
+        0 => None,
+        1 => {
+            let b = unsafe { b.assume_init() };
+            let l = unsafe { l.assume_init() };
 
-        Some(unsafe { core::slice::from_raw_parts(b, l) })
+            Some(unsafe { core::slice::from_raw_parts(b, l) })
+        }
+        r => {
+            log::error!("build_id__get returned error: {}", r);
+            None
+        }
     }
 }
