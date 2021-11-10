e2ee35cecce04bd6a522d0245ff30a33e23cd8ed
Cody P Schafer <dev@codyps.com>
2021-11-10 00:43:03 -0500

    test specifics

--- src/symbol-start-end.rs
+++ src/symbol-start-end.rs
@@ -1,13 +1,13 @@
 extern "C" {
-    static __build_id_start: u8;
-    static __build_id_end: u8;
+    static __build_id_start: [u8; 1];
+    static __build_id_end: [u8; 1];
 }
 
 pub fn build_id() -> Option<&'static [u8]> {
     unsafe {
-        let start = (&__build_id_start) as *const u8;
-        let end = (&__build_id_end) as *const u8;
-        let l = end.offset_from(start).try_into().unwrap();
-        Some(core::slice::from_raw_parts(start, l))
+        let start = __build_id_start.as_ptr();
+        let end = __build_id_end.as_ptr();
+        let len = end.offset_from(start).try_into().unwrap();
+        Some(core::slice::from_raw_parts(start, len))
     }
 }
