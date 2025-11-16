dc52546734467d207d8b7fd8d5dbcbd5a382965f
Cody P Schafer <dev@codyps.com>
2025-11-15 23:31:16 -0500

    fix: add test for macos using otool

--- tests/simple.rs
+++ tests/simple.rs
@@ -9,12 +9,47 @@ fn expected_build_id() -> Option<Vec<u8>> {
 
 #[test]
 fn has_build_id() {
-    env_logger::init();
     let id = buildid::build_id().unwrap();
     assert!(!id.is_empty());
 
     println!("{}", hex::encode(id));
     if let Some(expected_id) = expected_build_id() {
         assert_eq!(expected_id, id);
+    }
+}
+
+#[cfg(all(target_family = "unix", target_vendor = "apple",))]
+mod mach {
+    fn otool_uuid(exe_path: &std::path::Path) -> Option<Vec<u8>> {
+        use std::process::Command;
+        let otool_l = Command::new("otool")
+            .arg("-l")
+            .arg(exe_path)
+            .output()
+            .unwrap();
+        let mut lines = otool_l.stdout.split(|f| f == &b'\n');
+        while let Some(line) = lines.next() {
+            if line == b"     cmd LC_UUID" {
+                // should be " cmdsize 24", then the uuid
+                lines.next().expect("expected cmdsize line");
+                let uuid_line = std::str::from_utf8(lines.next().expect("expected uuid line"))
+                    .expect("invalid utf8");
+
+                let parts: Vec<&str> = uuid_line.trim().split_whitespace().collect();
+                assert!(parts.len() == 2 && parts[0] == "uuid");
+                let uuid_str = parts[1].replace("-", "");
+                return Some(hex::decode(uuid_str).unwrap());
+            }
+        }
+        None
+    }
+
+    #[test]
+    fn has_build_id_mach() {
+        let exe_path = std::env::current_exe().unwrap();
+        let uuid = otool_uuid(exe_path.as_ref()).unwrap();
+
+        let id = buildid::build_id().unwrap();
+        assert_eq!(uuid, id);
     }
 }
